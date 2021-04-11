3b044e20334ec232a177aa313e9594647b631f9a
Yacine1998 <yachab@myges.fr>
2021-04-11 14:32:27 +0200

    Ici xest le deucieme commit bravhe yacine

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import urllib.request
 from bs4 import BeautifulSoup
-
-
+print('edfg')
 print("Welcomeé to Projèct NoSQL \n   First Part : Scraping  \n ")
 
 url = urllib.request.urlopen('https://fr.wikipedia.org/wiki/D%C3%A9mographie_de_la_France/')
 soup = BeautifulSoup(url, 'html.parser')
 print(soup)
 print("braxhe yaxine")
-
-
-
+#dfvbgf
 
 
